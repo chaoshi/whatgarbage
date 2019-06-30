f0239d6d0a3470d0eced13e80d7263e333fa69c7
Chao Shi <mrchaoshi@gmail.com>
2019-06-29 23:03:24 -0400

    Updates

--- precache-manifest.926e7d430768eaba7c9012820b74ce8b.js
+++ precache-manifest.926e7d430768eaba7c9012820b74ce8b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/whatgarbage/static/js/runtime~main.4dc16f21.js"
   },
   {
-    "revision": "418dd6e51d63ac785085",
-    "url": "/whatgarbage/static/js/main.418dd6e5.chunk.js"
+    "revision": "21469e5096e16cd2dbea",
+    "url": "/whatgarbage/static/js/main.21469e50.chunk.js"
   },
   {
     "revision": "4f6ef1ce88de7701ff72",
     "url": "/whatgarbage/static/js/1.4f6ef1ce.chunk.js"
   },
   {
-    "revision": "418dd6e51d63ac785085",
+    "revision": "21469e5096e16cd2dbea",
     "url": "/whatgarbage/static/css/main.01c827f1.chunk.css"
   },
   {
-    "revision": "89a7026c1ec90eb5d01f97599985b106",
+    "revision": "8f9f54c73404e6996fc58ec9649720bd",
     "url": "/whatgarbage/index.html"
   }
 ];
