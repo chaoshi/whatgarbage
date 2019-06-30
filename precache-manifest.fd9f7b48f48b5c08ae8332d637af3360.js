9b6dcee3f7079caab749aacf34b7121f7a69d9a4
Chao Shi <mrchaoshi@gmail.com>
2019-06-30 17:24:26 -0400

    Updates

--- precache-manifest.fd9f7b48f48b5c08ae8332d637af3360.js
+++ precache-manifest.fd9f7b48f48b5c08ae8332d637af3360.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/whatgarbage/static/js/runtime~main.4dc16f21.js"
   },
   {
-    "revision": "6b9079adcd120d927ad9",
-    "url": "/whatgarbage/static/js/main.6b9079ad.chunk.js"
+    "revision": "9ee6526b65f33476c45a",
+    "url": "/whatgarbage/static/js/main.9ee6526b.chunk.js"
   },
   {
     "revision": "d996fe9740d9684532d8",
     "url": "/whatgarbage/static/js/1.d996fe97.chunk.js"
   },
   {
-    "revision": "6b9079adcd120d927ad9",
-    "url": "/whatgarbage/static/css/main.8c205e8e.chunk.css"
+    "revision": "9ee6526b65f33476c45a",
+    "url": "/whatgarbage/static/css/main.8e33c9c5.chunk.css"
   },
   {
-    "revision": "03f54059ae3eb3baa4b9c18ed2cfa796",
+    "revision": "5e71bd475d9ba55831d4e948b7eb6c11",
     "url": "/whatgarbage/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/whatgarbage/precache-manifest.4016673d9ef9abee40a4a7dc9ccffcf5.js"
+  "/whatgarbage/precache-manifest.fd9f7b48f48b5c08ae8332d637af3360.js"
 );
 
 workbox.clientsClaim();
