3670f3e743f14b061eeb26224cf762ee6abb6d27
Chao Shi <mrchaoshi@gmail.com>
2019-06-30 22:29:00 -0400

    Updates

--- precache-manifest.fad591eae56fe0ed796170f03beaf19a.js
+++ precache-manifest.fad591eae56fe0ed796170f03beaf19a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/whatgarbage/static/js/runtime~main.4dc16f21.js"
   },
   {
-    "revision": "d15cd86234cbbd1f8f39",
-    "url": "/whatgarbage/static/js/main.d15cd862.chunk.js"
+    "revision": "a12fcbc4a96e1bcd7f14",
+    "url": "/whatgarbage/static/js/main.a12fcbc4.chunk.js"
   },
   {
     "revision": "d996fe9740d9684532d8",
     "url": "/whatgarbage/static/js/1.d996fe97.chunk.js"
   },
   {
-    "revision": "d15cd86234cbbd1f8f39",
+    "revision": "a12fcbc4a96e1bcd7f14",
     "url": "/whatgarbage/static/css/main.8e33c9c5.chunk.css"
   },
   {
-    "revision": "6a3df57c2acb9deccfdd4b57a186c3af",
+    "revision": "ec2f9938f603e9a3d2bd736fbe635b0b",
     "url": "/whatgarbage/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/whatgarbage/precache-manifest.56c8ffe7791e27144b50090dd404a4ef.js"
+  "/whatgarbage/precache-manifest.fad591eae56fe0ed796170f03beaf19a.js"
 );
 
 workbox.clientsClaim();
